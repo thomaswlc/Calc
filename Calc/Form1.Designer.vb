7043371fca59562857b2155aa962bc15b8869a7b
11169007 <11169007@H22486D>
2023-09-20 12:09:40 +0100

    Add project files.

---
+++ Calc/Form1.Designer.vb
@@ -0,0 +1,141 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.cmdAdd = New System.Windows.Forms.Button()
+        Me.cmdSubtract = New System.Windows.Forms.Button()
+        Me.cmdMultiply = New System.Windows.Forms.Button()
+        Me.cmdDivide = New System.Windows.Forms.Button()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        Me.lblAnswer = New System.Windows.Forms.Label()
+        Me.txtFirst = New System.Windows.Forms.TextBox()
+        Me.txtSecond = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'cmdAdd
+        '
+        Me.cmdAdd.Location = New System.Drawing.Point(365, 77)
+        Me.cmdAdd.Name = "cmdAdd"
+        Me.cmdAdd.Size = New System.Drawing.Size(116, 39)
+        Me.cmdAdd.TabIndex = 0
+        Me.cmdAdd.Text = "Add"
+        Me.cmdAdd.UseVisualStyleBackColor = True
+        '
+        'cmdSubtract
+        '
+        Me.cmdSubtract.Location = New System.Drawing.Point(365, 122)
+        Me.cmdSubtract.Name = "cmdSubtract"
+        Me.cmdSubtract.Size = New System.Drawing.Size(116, 39)
+        Me.cmdSubtract.TabIndex = 1
+        Me.cmdSubtract.Text = "Subtract"
+        Me.cmdSubtract.UseVisualStyleBackColor = True
+        '
+        'cmdMultiply
+        '
+        Me.cmdMultiply.Location = New System.Drawing.Point(365, 167)
+        Me.cmdMultiply.Name = "cmdMultiply"
+        Me.cmdMultiply.Size = New System.Drawing.Size(116, 39)
+        Me.cmdMultiply.TabIndex = 2
+        Me.cmdMultiply.Text = "Multiply"
+        Me.cmdMultiply.UseVisualStyleBackColor = True
+        '
+        'cmdDivide
+        '
+        Me.cmdDivide.Location = New System.Drawing.Point(365, 212)
+        Me.cmdDivide.Name = "cmdDivide"
+        Me.cmdDivide.Size = New System.Drawing.Size(116, 39)
+        Me.cmdDivide.TabIndex = 3
+        Me.cmdDivide.Text = "Divide"
+        Me.cmdDivide.UseVisualStyleBackColor = True
+        '
+        'lblTitle
+        '
+        Me.lblTitle.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitle.Location = New System.Drawing.Point(120, 9)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(253, 41)
+        Me.lblTitle.TabIndex = 4
+        Me.lblTitle.Text = "CALCULATOR"
+        '
+        'lblAnswer
+        '
+        Me.lblAnswer.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblAnswer.Location = New System.Drawing.Point(30, 261)
+        Me.lblAnswer.Name = "lblAnswer"
+        Me.lblAnswer.Size = New System.Drawing.Size(109, 41)
+        Me.lblAnswer.TabIndex = 5
+        Me.lblAnswer.Text = "Answer"
+        '
+        'txtFirst
+        '
+        Me.txtFirst.Location = New System.Drawing.Point(52, 96)
+        Me.txtFirst.Name = "txtFirst"
+        Me.txtFirst.Size = New System.Drawing.Size(100, 20)
+        Me.txtFirst.TabIndex = 6
+        '
+        'txtSecond
+        '
+        Me.txtSecond.Location = New System.Drawing.Point(52, 141)
+        Me.txtSecond.Name = "txtSecond"
+        Me.txtSecond.Size = New System.Drawing.Size(100, 20)
+        Me.txtSecond.TabIndex = 7
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(52, 185)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 8
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(493, 361)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.txtSecond)
+        Me.Controls.Add(Me.txtFirst)
+        Me.Controls.Add(Me.lblAnswer)
+        Me.Controls.Add(Me.lblTitle)
+        Me.Controls.Add(Me.cmdDivide)
+        Me.Controls.Add(Me.cmdMultiply)
+        Me.Controls.Add(Me.cmdSubtract)
+        Me.Controls.Add(Me.cmdAdd)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents cmdAdd As Button
+    Friend WithEvents cmdSubtract As Button
+    Friend WithEvents cmdMultiply As Button
+    Friend WithEvents cmdDivide As Button
+    Friend WithEvents lblTitle As Label
+    Friend WithEvents lblAnswer As Label
+    Friend WithEvents txtFirst As TextBox
+    Friend WithEvents txtSecond As TextBox
+    Friend WithEvents TextBox1 As TextBox
+End Class
---
+++ Calc/Form1.vb
@@ -0,0 +1,73 @@
+﻿Public Class Form1
+    Private Sub cmdAdd_Click(sender As Object, e As EventArgs) Handles cmdAdd.Click
+        'declare variables
+        Dim answer As Integer
+        Dim firstNumber As Integer
+        Dim secondNumber As Integer
+
+        'input
+        firstNumber = txtFirst.Text
+        secondNumber = txtSecond.Text
+
+        'processing
+        answer = firstNumber + secondNumber
+
+        'output
+        lblAnswer.Text = answer
+
+    End Sub
+
+    Private Sub cmdSubtract_Click(sender As Object, e As EventArgs) Handles cmdSubtract.Click
+        'declare variables
+        Dim answer As Integer
+        Dim firstNumber As Integer
+        Dim secondNumber As Integer
+
+        'input
+        firstNumber = txtFirst.Text
+        secondNumber = txtSecond.Text
+
+        'processing
+        answer = firstNumber - secondNumber
+
+        'output
+        lblAnswer.Text = answer
+
+    End Sub
+
+    Private Sub cmdMultiply_Click(sender As Object, e As EventArgs) Handles cmdMultiply.Click
+        'declare variables
+        Dim answer As Integer
+        Dim firstNumber As Integer
+        Dim secondNumber As Integer
+
+        'input
+        firstNumber = txtFirst.Text
+        secondNumber = txtSecond.Text
+
+        'processing
+        answer = firstNumber * secondNumber
+
+        'output
+        lblAnswer.Text = answer
+
+    End Sub
+
+    Private Sub cmdDivide_Click(sender As Object, e As EventArgs) Handles cmdDivide.Click
+        'declare variables
+        Dim answer As Integer
+        Dim firstNumber As Integer
+        Dim secondNumber As Integer
+
+        'input
+        firstNumber = txtFirst.Text
+        secondNumber = txtSecond.Text
+
+        'processing
+        answer = firstNumber / secondNumber
+
+        'output
+        lblAnswer.Text = answer
+
+    End Sub
+End Class
